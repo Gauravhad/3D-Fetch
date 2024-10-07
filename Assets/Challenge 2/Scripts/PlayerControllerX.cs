deb9202b931de321a6687e1e6e69974125630375
Gauravhad <gauravhad22@gmail.com>
2024-10-07 15:58:42 +0530

    First Commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delayTime = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        delayTime -= 0.01f;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && delayTime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+             delayTime = 2.0f;
+        }
+    }
+}
